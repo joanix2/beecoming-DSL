db16aa2179d7be449342e53ac299deae29a09e6c
Joan DUSSAULD <joan.dussauld@etu.univ-nantes.fr>
2026-02-12 10:33:59 +0100

    clean front

---
+++ TODO.md
@@ -0,0 +1,6 @@
+clean back
+clean front
+rechercher les types
+scaffolding
+ajout LLMs
+créer un AGENTS.md
--- back/templates/front/android/app/src/main/java/fr/beecoming/opteeam/dev/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package fr.beecoming.opteeam.dev;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
